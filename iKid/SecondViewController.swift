43a718ed43e1c9dcc0d55e8e4549e5c3efbaf794
applemac <applemac@macdemacbook-pro-5.local>
2018-10-27 20:38:52 -0700

    Initial Commit

---
+++ iKid/SecondViewController.swift
@@ -0,0 +1,20 @@
+//
+//  SecondViewController.swift
+//  iKid
+//
+//  Created by applemac on 10/27/18.
+//  Copyright © 2018 AllenShi. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
+  }
+
+
+}
+
